0c980a79f1e3b878a62c5ae008c1243516be4af4
Freddy Drennan <drennanfreddy@gmail.com>
2019-10-06 17:29:25 -0600

    update

--- R/connectr_init.R
+++ R/connectr_init.R
@@ -6,17 +6,17 @@ connectr_init <- function() {
   # Create connectr directory
   connectr_dir <- '~/.connectr'
   if (!dir.exists(connectr_dir)) {
-    packageStartupMessage('Creating base directory for connectr at ~/.connectr')
+    message('Creating base directory for connectr at ~/.connectr')
     dir.create(connectr_dir)
   } else {
-    packageStartupMessage('Using base directory for connectr at ~/.connectr')
+    message('Using base directory for connectr at ~/.connectr')
   }
 
   # Create default config file.
   config_name <-  'config.ini'
   config_path <- file.path(connectr_dir, config_name)
   if (!file.exists(config_path)) {
-    packageStartupMessage(glue('Creating default config.ini file at {config_path}'))
+    message(glue('Creating default config.ini file at {config_path}'))
     write_file(config_default, config_path)
   }
 }
