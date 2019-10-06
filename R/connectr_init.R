e2e0571166c82ba7b49719f561ab16c0dd64dbb8
Freddy Drennan <drennanfreddy@gmail.com>
2019-10-06 17:26:55 -0600

    -update

--- R/connectr_init.R
+++ R/connectr_init.R
@@ -1,6 +1,9 @@
 #' @importFrom readr write_file
 #' @importFrom glue glue
-.onLoad <- function(libname, pkgname){
+#' @export connectr_init
+connectr_init <- function() {
+
+  # Create connectr directory
   connectr_dir <- '~/.connectr'
   if (!dir.exists(connectr_dir)) {
     packageStartupMessage('Creating base directory for connectr at ~/.connectr')
@@ -9,13 +12,11 @@
     packageStartupMessage('Using base directory for connectr at ~/.connectr')
   }
 
+  # Create default config file.
   config_name <-  'config.ini'
   config_path <- file.path(connectr_dir, config_name)
-
   if (!file.exists(config_path)) {
     packageStartupMessage(glue('Creating default config.ini file at {config_path}'))
-    starting_configuration <- "[localhost_postgres]\ndbname = postgres\nhost = localhost\nport = 5432\nuser = postgres\npassword = docker\n"
     write_file(config_default, config_path)
   }
 }
-
